a6f911a5241784f47e9930a6a1892a8b2073e7e7
pradhigda71 <yaniarpradhigda@gmail.com>
2018-05-23 16:02:48 +0700

    jaring-script

---
+++ post.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    // process the form
+    $('form').submit(function (event) {
+
+        // get the form data
+        // there are many ways to get this data using jQuery (you can use the class or id also)
+        var formData = {
+            'name': $('input[name=name]').val(),
+            'address': $('input[name=address]').val(),
+            'email': $('input[name=email]').val(),
+            'phone': $('input[name=phone]').val(),
+            'city': $('select[name=city]').val(),
+            'gender': $('select[name=gender]').val(),
+            'source': $('input[name=source]').val()
+
+        };
+
+        // process the form
+        $.ajax({
+            type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url: 'https://jaringlead.com/new-jaring/api/getLead/', // the url where we want to POST
+            data: formData, // our data object
+            dataType: 'json', // what type of data do we expect back from the server
+            encode: true
+        })
+                // using the done promise callback
+                .done(function (data) {
+
+                    // log data to the console so we can see
+                    //console.log(data);
+                    if (data.res) {
+                        alert('thanks for your interest');
+                    } else {
+                        alert('sorry the system has been error');
+                    }
+                    window.location.href = data.redirect;
+                    // here we will handle errors and validation messages
+                });
+
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
+    });
+
+});
---
+++ tracking.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+
+    var q = getUrlParameter('q');
+    var url = "https://jaringlead.com/new-jaring/tracking/" ;
+
+    if(typeof q != 'undefined'){
+        var data = {
+        'query' : q
+        }
+
+        $.ajax({
+            type: "POST",
+            dataType: 'json',
+            url: url,
+            data: data,
+            success: function (data) {
+                //window.location.href= data.redirect
+                $('#source').val(data.unique_code);
+                $('select[name=city]').html(data.htmlCity)
+                //console.log(data);
+            }
+        });
+
+    }
+});
